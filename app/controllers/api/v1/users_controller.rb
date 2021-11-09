b49b74608965a2477fdbc3f5824587e510b0cb42
muhenge <mugunga5@gmail.com>
2021-11-09 16:00:09 +0200

    Adds authenticable module for managing authentication methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { user: @user, message: "Account created successfully!" }. status: :created
+      render json: { user: @user, message: "Account created successfully!" } status: :created
     else
       render json: {@user.errors}, status: :unprocessable_entity
     end
