eee52fa54d1bbdc6c532d405ee7202503540daf3
muhenge <mugunga5@gmail.com>
2021-11-09 17:18:55 +0200

    fixed some bugs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: { users: @users }, status: :ok
   end
-  end
+  
   def show
     render json: @user
   end
@@ -12,9 +12,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { user: @user, message: "Account created successfully!" } status: :created
+      render json: { user: @user, message: "Account created successfully!" }, status: :created
     else
-      render json: {@user.errors}, status: :unprocessable_entity
+      render json: {user:@user.errors}, status: :unprocessable_entity
     end
   end
 
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
   end
   def destroy
     @user.destroy
-    render json: {message: ""}, head 204
+    head 204
   end
   private
   def user_params
